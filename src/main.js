178523d6efd1d8af7864074cca5377c9f3026344
suntong233 <2654073941@qq.com>
2019-10-15 14:36:09 +0800

    stchanged

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import { Button } from "element-ui";
+import axios from "axios";
 
 Vue.use(Button);
 
-Vue.config.productionTip = false;
+Vue.prototype.axios = axios;
 
+Vue.config.productionTip = false;
 new Vue({
   router,
   store,
--- src/views/ExampleSt.vue
+++ src/views/ExampleSt.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h2>st{{ test }}</h2>
+    <div>{{ testArr }}</div>
   </div>
 </template>
 
@@ -9,8 +10,15 @@ export default {
   name: "st",
   data() {
     return {
-      test: '测试'
+      test: "测试",
+      testArr: []
     };
+  },
+  created() {
+    this.axios.get("/data/test.json").then(res => {
+      console.log(res.data);
+      this.testArr = res.data.arrdata;
+    });
   }
 };
 </script>
