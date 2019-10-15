ad695b2df6f05ee7c1fda9ce1db550609d464e9c
suntong233 <2654073941@qq.com>
2019-10-15 11:39:20 +0800

    test

---
+++ src/router.js
@@ -0,0 +1,20 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "*",
+      redirect: "/"
+    },
+    {
+      path: "/",
+      name: "home",
+      component: () => import("./views/Home.vue")
+    }
+  ]
+});
