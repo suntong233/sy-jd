106ca1e4aa2a5b3e99d7617858b203e9001d7bf5
yasuo <531176066@qq.com>
2019-10-16 18:37:59 +0800

    wsm

---
+++ src/components/wsm/advertui.vue
@@ -0,0 +1,273 @@
+<template>
+  <div class="advertui">
+    <div class="swiper-container advwsm">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide">
+          <div class="flex-ui">
+            <div class="advui" @click="$router.push('/')">
+              <img
+                src="//m.360buyimg.com/mobilecms/s120x120_jfs/t1/20983/16/10753/6124/5c8a16aaE5d6b15d7/01e0e818a7505267.png.webp"
+                alt
+                style="width:40px;height:auto"
+              />
+              <span>京东超市</span>
+            </div>
+            <div class="advui" @click="$router.push('/')">
+              <img
+                src="//m.360buyimg.com/mobilecms/s120x120_jfs/t1/39401/17/2391/5859/5cc06fcfE2ad40668/28cda0a571b4a576.png.webp"
+                alt
+                style="width:40px;height:auto"
+              />
+              <span>数码电器</span>
+            </div>
+            <div class="advui"  @click="$router.push('/')">
+              <img
+                src="//m.360buyimg.com/mobilecms/s120x120_jfs/t1/17169/3/4127/4611/5c2f35cfEd87b0dd5/65c0cdc1362635fc.png.webp"
+                alt
+                style="width:40px;height:auto"
+              />
+              <span>京东服饰</span>
+            </div>
+            <div class="advui"  @click="$router.push('/')">
+              <img
+                src="//m.360buyimg.com/mobilecms/s120x120_jfs/t17725/156/1767366877/17404/f45d418b/5ad87bf0N66c5db7c.png.webp"
+                alt
+                style="width:40px;height:auto"
+              />
+              <span>京东生鲜</span>
+            </div>
+            <div class="advui"  @click="$router.push('/')">
+              <img
+                src="//m.360buyimg.com/mobilecms/s120x120_jfs/t16990/157/2001547525/17770/a7b93378/5ae01befN2494769f.png.webp"
+                alt
+                style="width:40px;height:auto"
+              />
+              <span>京东到家</span>
+            </div>
+          </div>
+          <div class="flex-ui">
+            <div class="advui"  @click="$router.push('/')">
+              <img
+                src="//m.360buyimg.com/mobilecms/s120x120_jfs/t18454/342/2607665324/6406/273daced/5b03b74eN3541598d.png.webp"
+                alt
+                style="width:40px;height:auto"
+              />
+              <span>充值缴费</span>
+            </div>
+            <div class="advui"  @click="$router.push('/')">
+              <img
+                src="//m.360buyimg.com/mobilecms/s120x120_jfs/t22228/270/207441984/11564/88140ab7/5b03fae3N67f78fe3.png.webp"
+                alt
+                style="width:40px;height:auto"
+              />
+              <span>9.9元拼</span>
+            </div>
+            <div class="advui"  @click="$router.push('/')">
+              <img
+                src="//m.360buyimg.com/mobilecms/s120x120_jfs/t1/7068/29/8987/5605/5c120da2Ecae1cc3a/016033c7ef3e0c6c.png.webp"
+                alt
+                style="width:40px;height:auto"
+              />
+              <span>领券</span>
+            </div>
+            <div class="advui"  @click="$router.push('/')">
+              <img
+                src="//m.360buyimg.com/mobilecms/s120x120_jfs/t16828/63/2653634926/5662/d18f6fa1/5b03b779N5c0b196f.png.webp"
+                alt
+                style="width:40px;height:auto"
+              />
+              <span>赚钱</span>
+            </div>
+            <div class="advui"  @click="$router.push('/')">
+              <img
+                src="//m.360buyimg.com/mobilecms/s120x120_jfs/t1/22262/9/1470/4527/5c120cd0E5d3c6c66/4792ec307a853e9f.png.webp"
+                alt
+                style="width:40px;height:auto"
+              />
+              <span>PLUS会员</span>
+            </div>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="flex-ui">
+            <div class="advui"  @click="$router.push('/')">
+              <img
+                src="//m.360buyimg.com/mobilecms/s120x120_jfs/t1/20983/16/10753/6124/5c8a16aaE5d6b15d7/01e0e818a7505267.png.webp"
+                alt
+                style="width:40px;height:auto"
+              />
+              <span>京东超市</span>
+            </div>
+            <div class="advui"  @click="$router.push('/')">
+              <img
+                src="//m.360buyimg.com/mobilecms/s120x120_jfs/t1/39401/17/2391/5859/5cc06fcfE2ad40668/28cda0a571b4a576.png.webp"
+                alt
+                style="width:40px;height:auto"
+              />
+              <span>数码电器</span>
+            </div>
+            <div class="advui"  @click="$router.push('/')">
+              <img
+                src="//m.360buyimg.com/mobilecms/s120x120_jfs/t1/17169/3/4127/4611/5c2f35cfEd87b0dd5/65c0cdc1362635fc.png.webp"
+                alt
+                style="width:40px;height:auto"
+              />
+              <span>京东服饰</span>
+            </div>
+            <div class="advui"  @click="$router.push('/')">
+              <img
+                src="//m.360buyimg.com/mobilecms/s120x120_jfs/t17725/156/1767366877/17404/f45d418b/5ad87bf0N66c5db7c.png.webp"
+                alt
+                style="width:40px;height:auto"
+              />
+              <span>京东生鲜</span>
+            </div>
+            <div class="advui"  @click="$router.push('/')">
+              <img
+                src="//m.360buyimg.com/mobilecms/s120x120_jfs/t16990/157/2001547525/17770/a7b93378/5ae01befN2494769f.png.webp"
+                alt
+                style="width:40px;height:auto"
+              />
+              <span>京东到家</span>
+            </div>
+          </div>
+          <div class="flex-ui">
+            <div class="advui"  @click="$router.push('/')">
+              <img
+                src="//m.360buyimg.com/mobilecms/s120x120_jfs/t18454/342/2607665324/6406/273daced/5b03b74eN3541598d.png.webp"
+                alt
+                style="width:40px;height:auto"
+              />
+              <span>充值缴费</span>
+            </div>
+            <div class="advui"  @click="$router.push('/')">
+              <img
+                src="//m.360buyimg.com/mobilecms/s120x120_jfs/t22228/270/207441984/11564/88140ab7/5b03fae3N67f78fe3.png.webp"
+                alt
+                style="width:40px;height:auto"
+              />
+              <span>9.9元拼</span>
+            </div>
+            <div class="advui"  @click="$router.push('/')">
+              <img
+                src="//m.360buyimg.com/mobilecms/s120x120_jfs/t1/7068/29/8987/5605/5c120da2Ecae1cc3a/016033c7ef3e0c6c.png.webp"
+                alt
+                style="width:40px;height:auto"
+              />
+              <span>领券</span>
+            </div>
+            <div class="advui"  @click="$router.push('/')">
+              <img
+                src="//m.360buyimg.com/mobilecms/s120x120_jfs/t16828/63/2653634926/5662/d18f6fa1/5b03b779N5c0b196f.png.webp"
+                alt
+                style="width:40px;height:auto"
+              />
+              <span>赚钱</span>
+            </div>
+            <div class="advui"  @click="$router.push('/')">
+              <img
+                src="//m.360buyimg.com/mobilecms/s120x120_jfs/t1/22262/9/1470/4527/5c120cd0E5d3c6c66/4792ec307a853e9f.png.webp"
+                alt
+                style="width:40px;height:auto"
+              />
+              <span>PLUS会员</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class=" swiper-pagination nagtion"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { type } from "os";
+export default {
+  name: "advertui",
+  methods: {},
+  mounted() {
+     new Swiper(".advwsm", {
+        speed:800,
+        resistanceRatio : 0,
+      autoplay: true,
+      pagination: {
+        el: ".advertui .swiper-pagination",
+        type: "custom",
+        renderCustom: function(swiper, current, total) {
+          var paginationHtml = " ";
+          for (var i = 0; i < total; i++) {
+            // 判断是不是激活焦点，是的话添加active类，不是就只添加基本样式类
+            if (i === current - 1) {
+              paginationHtml +=
+                '<span class="swiper-pagination-customs swiper-pagination-customs-active"></span>';
+            } else {
+              paginationHtml +=
+                '<span class="swiper-pagination-customs"></span>';
+            }
+          }
+          return paginationHtml;
+        }
+      }
+    });
+  }
+};
+</script>
+
+<style>
+.advertui {
+  width: 100%;
+
+  padding: 5px 8px;
+}
+.advertui > .swiper-container {
+  height: 180px;
+}
+.flex-ui {
+  width: 100%;
+
+  display: flex;
+  justify-content: space-between;
+}
+
+/*包裹自定义分页器的div的位置等CSS样式*/
+.nagtion .swiper-pagination-custom {
+  bottom: 5%;
+  left: 0;
+  width: 100%;
+  height: 20px;
+  /* background-color: red; */
+  text-align: center;
+}
+/*自定义分页器的样式，这个你自己想要什么样子自己写*/
+.nagtion .swiper-pagination-customs {
+  width:5px;
+  height:5px;
+  display: inline-block;
+  border:1px ;
+  border-radius:25px;
+  background: rgb(237, 237, 237, 0.4);
+  opacity: 0.3;
+  /* box-shadow: 0 0 2px  #000; */
+  margin: 0 5px;
+  outline: 0;
+}
+/*自定义分页器激活时的样式表现*/
+.nagtion .swiper-pagination-customs-active {
+  opacity: 1;
+  width: 12px;
+  height: 5px;
+  border-radius:25px;
+  border: 3px solid #ED7375;
+  background-color: #ED7375;
+}
+.advui {
+  padding: 8px;
+  width: 82px;
+  height: 80px;
+  text-align: center;
+}
+.advui > span {
+  display: block;
+  font-size: 0.6rem;
+}
+</style>
---
+++ src/components/wsm/indexbanner.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="indexbanner">
+    <div class="swiper-container indexwsm">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide">
+          <img
+            src="//m.360buyimg.com/mobilecms/s700x280_jfs/t1/64884/35/12645/75271/5d9ecd56E23cdc7c9/c6afaee40d1ab1ba.jpg!cr_1125x445_0_171!q70.jpg.dpg"
+            alt
+            style="width:385px;height:auto"
+          />
+        </div>
+        <div class="swiper-slide">
+          <img
+            src="//m.360buyimg.com/mobilecms/s700x280_jfs/t1/76725/4/12876/109902/5da3f2c9E56f02d7c/cd7e216f3d249d09.jpg!cr_1125x445_0_171!q70.jpg.dpg"
+            alt
+            style="width:385px;height:auto"
+          />
+        </div>
+        <div class="swiper-slide">
+          <img
+            src="//m.360buyimg.com/mobilecms/s700x280_jfs/t1/63231/9/13147/148922/5da589f4E8d6b4b3b/cdf00f92d620b170.jpg!cr_1125x445_0_171!q70.jpg.dpg"
+            alt
+            style="width:385px;height:auto"
+          />
+        </div>
+      </div>
+        <div class="swiper-pagination homebanner"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { type } from "os";
+export default {
+  name: "indexbanner",
+  methods: {},
+  mounted() {
+    var mySwiper = new Swiper(".indexwsm", {
+      resistanceRatio: 0,
+      pagination: {
+        el: ".indexbanner .swiper-pagination",
+        type: "custom",
+        renderCustom: function(swiper, current, total) {
+          var paginationHtml = " ";
+          for (var i = 0; i < total; i++) {
+            // 判断是不是激活焦点，是的话添加active类，不是就只添加基本样式类
+            if (i === current - 1) {
+              paginationHtml +=
+                '<span class="swiper-pagination-customs swiper-pagination-customs-active"></span>';
+            } else {
+              paginationHtml +=
+                '<span class="swiper-pagination-customs"></span>';
+            }
+          }
+          return paginationHtml;
+        }
+      },
+      autoplay: true,
+      loop: true
+    });
+  }
+};
+</script>
+
+<style>
+.indexbanner {
+  width: 100%;
+  height: 155px;
+  padding: 5px 8px;
+}
+.swiper-container {
+    width: 100%;
+    height: 100%;
+  border-radius: 8px;
+}
+/*包裹自定义分页器的div的位置等CSS样式*/
+ .homebanner .swiper-pagination-custom {
+  bottom: 5%;
+  left: 0;
+  width: 100%;
+  height: 20px;
+  /* background-color: red; */
+  text-align: center;
+}
+/*自定义分页器的样式，这个你自己想要什么样子自己写*/
+.homebanner .swiper-pagination-customs {
+  width: 15px;
+  height: 3px;
+  display: inline-block;
+  background: rgb(237, 237, 237, 0.4);
+  opacity: 0.3;
+  /* box-shadow: 0 0 2px  #000; */
+  margin: 0 5px;
+  outline: 0;
+}
+/*自定义分页器激活时的样式表现*/
+.homebanner .swiper-pagination-customs-active {
+  opacity: 1;
+  border: 3px solid #8e2829;
+  background-color: #8e2829;
+}
+</style>
