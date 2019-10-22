99871d1c1fde3dc0b67ce1d4d775178e1883570d
lz <lz005678@163.com>
2019-10-22 18:42:55 +0800

    lz

--- src/components/lz/goodsdetailtop1.vue
+++ src/components/lz/goodsdetailtop1.vue
@@ -3,22 +3,22 @@
         <div class="goods_detail_top_container1">
                 <div class="leftdiv1" @click="lzback1"><span class="el-icon-arrow-left"></span></div>
                 <div class="goods_detail_top_container1_middle">
-                    <div style="color:#e4393c">商品</div>
-                    <div>评价</div>
-                    <div>详情</div>
-                    <div>推荐</div>
+                    <div :class="{lzactive100:lzlzmsg1==1}" onclick="window.scrollTo(0, 0)">商品</div>
+                    <div :class="{lzactive100:lzlzmsg1==2}" onclick="window.scrollTo(0, 865)">评价</div>
+                    <div :class="{lzactive100:lzlzmsg1==3}" onclick="window.scrollTo(0, 2283)">详情</div>
+                    <div :class="{lzactive100:lzlzmsg1==4}" onclick="window.scrollTo(0, 1797)">推荐</div>
                 </div>
                 <div class="leftdiv2" @click="lzshow1 = !lzshow1"><span  class="etc1">···</span></div>
         </div>
         <showMenu v-if="lzshow1"></showMenu>
   </div>
 </template>
-
 <script>
 import showMenu from "../public/showMenu.vue";   // 显示隐藏菜单
 export default {
     data(){ return{
-         lzshow1:false
+         lzshow1:false,
+         lzlzmsg1:1
     }
        
     },
@@ -29,6 +29,22 @@ export default {
      },
      components: {
          showMenu
+     },
+     mounted () {
+           window.addEventListener("scroll", () => {
+              if(window.scrollY < 865){
+                  this.lzlzmsg1 =1
+              }
+              if(window.scrollY >= 865 & window.scrollY < 1515){
+                  this.lzlzmsg1 =2
+              }
+              if(window.scrollY >= 1797 & window.scrollY < 2267){
+                  this.lzlzmsg1 =4
+              }
+              if(window.scrollY >=2283){
+                  this.lzlzmsg1 =3
+              }
+        });
      }
 }
 </script>
@@ -41,6 +57,9 @@ export default {
         border: 1px solid #f5f5f5
     }
 
+    .lzactive100{
+        color: #e4393c
+    }
     .goods_detail_top_container1{
         width: 100%;
         display: flex;
@@ -99,7 +118,6 @@ export default {
          justify-content: center;
          align-items: center;
          font-size: 12px;
-         color: #252525;
      }
   
 </style>
